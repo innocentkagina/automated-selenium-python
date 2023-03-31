7b1f5f3fc54424d23dc9dab77c7ed20422cdec5e
innocentkagina <kaginno2000@gmail.com>
2023-04-01 00:17:14 +0300

    updated index

--- index.py
+++ index.py
@@ -1,9 +1,5 @@
-from ast import Bytes
 import time
-import random
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from faker import Faker
 
